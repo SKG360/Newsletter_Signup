34e8010ce4f915586b810b6ea7e99ef7cadfc2e6
SKG360 <samuel.giles@colorado.edu>
2020-04-24 17:13:20 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(express.static("public")); // use this to make static images and files available to the server.
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req, res) { // 'req' comes from client via the form
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data); // flatten out the 'data' varible from line 23.
+  const url = 'https://us8.api.mailchimp.com/3.0/lists/117777a8a4';
+  const options = {
+    method: "POST",
+    auth: "skg360:fffe74378d537742b5b215fd2bbef892-us8"
+  };
+
+  const request = https.request(url, options, function(response) { //response is an object
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) { //use 'on' method on 'response' to search thru server data in response
+      console.log(JSON.parse(data)); // transforms server data into javascript object
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+  console.log(firstName, lastName, email);
+});
+
+app.post('/failure', function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on Port 3000.")
+});
+
+// API Key
+// fffe74378d537742b5b215fd2bbef892-us8
+
+// List Id
+// 117777a8a4
